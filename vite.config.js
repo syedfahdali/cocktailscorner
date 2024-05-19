c7b1ac6a3b62a76bdb3f581faad6c35ed6f28b3c
syedfahdali <syedfahd999@gmail.com>
2024-05-19 15:02:12 +0500

    made crucial changes to vite config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/cocktailvite"
-})
-  
+  base: "/", // Update the base URL to match your GitHub repository name
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'), // Define alias for src directory
+    },
+  },
+  server: {
+    port: 5173, // Specify the development server port
+  },
+});
